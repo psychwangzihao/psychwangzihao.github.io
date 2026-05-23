039c1929e0548bee508265bd7d5c8e4e19c7e65d
2534Henry <1022182060@qq.com>
2026-05-23 03:45:40 +0000

    Deploying to gh-pages from @ 2534Henry/2534Henry.github.io@6cdc3ebcd978437cd45778ad22f34d4b8d51b701 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,13 +30,6 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
         },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
